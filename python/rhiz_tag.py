e18e92a917115f322c25ba19d645945ebfa29cc7
Jean-Louis Fuchs <safe.pen2857@rhizoome.ch>
2024-09-26 00:56:10 +0200

    update cli description

--- python/rhiz_tag.py
+++ python/rhiz_tag.py
@@ -89,20 +89,14 @@ try:
     @click.group()
     def cli():
         """
-        Generate a Ubiquitous General Purpose Tag to use in your notes,
-        zettelkasten, field research or archival project (photos, drafts,
-        scrapbooking). Use it as a date-identifier for your note's header or as a
-        reference. You can use it between media: Digital, typewritten, or
-        handwritten. Reference a note in a book, in a paper, or in your source-
-        code. Everything with the same tag is connected.
+        Generate a universal, general-purpose tag to use in your notes, Zettelkasten,
+        field research, or archival projects (photos, drafts, scrapbooking). Use it as a
+        date identifier for your note's header or as a reference. You can apply it
+        across different media: Digital, typewritten, or handwritten. Reference a note
+        in a book, a paper, or in your source code. Everything with the same tag is
+        connected.
 
-        The tag contains a date, based on base54: The first letter is the year,
-        beginning from 2024, the second letter is the week of the year using ISO
-        weeks. The third letter is a 52nd of a week. It's primarily meant to
-        give the tags an order, but if you use the tags daily, you learn to read
-        them.
-
-        For more details please see the documentation.
+        For more details please see the [documentation](https://github.com/rhizoome/rhiz_tag).
         """
         pass
 
